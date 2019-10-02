095f6dd3cd6b8e2846aebd5b9617d7c7ba69b82b
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-02 10:50:29 -0600

    Deletes base folder and changes xcode form 9.3 to 11

---
+++ JustBreath/Models/Response.swift
@@ -0,0 +1,11 @@
+//
+//  Response.swift
+//  JustBreath
+//
+//  Created by Kevin Tanner on 10/2/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
