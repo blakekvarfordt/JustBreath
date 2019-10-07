bf44d638ee26470d7d3715fc824a25cfd30d67bf
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-10-07 14:25:47 -0600

    Segues to request detail VC

--- JustBreateApp/JustBreateApp/View Controllers/Profile/ProfileViewController.swift
+++ JustBreateApp/JustBreateApp/View Controllers/Profile/ProfileViewController.swift
@@ -31,15 +31,14 @@ class ProfileViewController: UIViewController {
         pointsLabel.text = "\(100) KP Points"
     }
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
 
 }
 
