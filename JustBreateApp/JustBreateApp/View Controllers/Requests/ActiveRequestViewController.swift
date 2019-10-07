51347dcc947da6c04faf741c8b643f2a70ed030b
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-07 14:42:54 -0600

    sets up responses tableview cells

--- JustBreateApp/JustBreateApp/View Controllers/Requests/ActiveRequestViewController.swift
+++ JustBreateApp/JustBreateApp/View Controllers/Requests/ActiveRequestViewController.swift
@@ -25,6 +25,8 @@ class ActiveRequestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        responsesTableView.delegate = self
+        responsesTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
@@ -49,4 +51,48 @@ class ActiveRequestViewController: UIViewController {
     }
     */
 
+}
+
+extension ActiveRequestViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "responseCell", for: indexPath) as? ResponseTableViewCell else { return UITableViewCell()}
+        
+        let request = ProfileMockDataController3.shared.mockDataObjects[indexPath.row]
+        
+        cell.requestLandingPad = request
+        
+        return cell
+    }
+    
+    
+}
+
+
+class ProfileMockDataModel3 {
+    let text: String
+    let image: UIImage?
+    
+    init(text: String, image: UIImage?) {
+        self.text = text
+        self.image = image
+    }
+}
+
+class ProfileMockDataController3 {
+    static let shared = ProfileMockDataController3()
+    
+    var mockDataObjects = [ProfileMockDataModel3]()
+    
+    init() {
+        
+        let request1 = ProfileMockDataModel3(text: "#whatever #amiseeingthings #iphoneForTheWin", image: UIImage(named: "mountain"))
+        let request2 = ProfileMockDataModel3(text: "#customTableViews #BadDay #WorkSucks", image: UIImage(named: "focus"))
+        let request3 = ProfileMockDataModel3(text: "#DoesItWork #WAterIsLife #RAinbow", image: UIImage(named: "canyonJump"))
+        
+        self.mockDataObjects = [request1, request2, request3]
+    }
 }
--- JustBreateApp/JustBreateApp/View Controllers/Requests/Custom Cells/ResponseTableViewCell.swift
+++ JustBreateApp/JustBreateApp/View Controllers/Requests/Custom Cells/ResponseTableViewCell.swift
@@ -20,6 +20,16 @@ class ResponseTableViewCell: UITableViewCell {
     @IBOutlet weak var blockUserButton: UIButton!
     @IBOutlet weak var bookmarkResponseButton: UIButton!
     
+    // MARK: - Properties
+    
+    var requestLandingPad: ProfileMockDataModel3? {
+        didSet {
+            layoutIfNeeded()
+            setupView()
+        }
+    }
+
+    
     // MARK: - Lifecycle Methods
 
     override func awakeFromNib() {
@@ -51,8 +61,9 @@ class ResponseTableViewCell: UITableViewCell {
     
     
     // MARK: - UI Adjustments
-
-
-
-
+    func setupView() {
+        responseImageView.image = requestLandingPad?.image
+        usernameLabel.text = "UserNAME"
+        responseBodyLabel.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+    }
 }
