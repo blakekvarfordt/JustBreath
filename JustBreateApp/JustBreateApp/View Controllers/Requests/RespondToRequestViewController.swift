41fc5ed50bf8eac2d7b8f6a78603691d71e0aad6
Cameron Stuart <apps@cameronstuart.com>
2019-10-08 09:59:25 -0600

    adds constraints to myRequests view.

--- JustBreateApp/JustBreateApp/View Controllers/Requests/RespondToRequestViewController.swift
+++ JustBreateApp/JustBreateApp/View Controllers/Requests/RespondToRequestViewController.swift
@@ -36,7 +36,7 @@ class RespondToRequestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addLinkTextField.isHidden = true
     }
     
     // MARK: - Actions
