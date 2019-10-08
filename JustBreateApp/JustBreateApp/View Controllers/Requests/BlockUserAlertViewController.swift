9acb93de2599634d17710382481fd4e1951c7323
Cameron Stuart <apps@cameronstuart.com>
2019-10-08 10:44:16 -0600

    adds popup views for blocking and reporting.

--- JustBreateApp/JustBreateApp/View Controllers/Requests/BlockUserAlertViewController.swift
+++ JustBreateApp/JustBreateApp/View Controllers/Requests/BlockUserAlertViewController.swift
@@ -12,19 +12,14 @@ class BlockUserAlertViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true)
     }
-    */
-
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
 }
--- JustBreateApp/JustBreateApp/View Controllers/Requests/FlagPostAlertViewController.swift
+++ JustBreateApp/JustBreateApp/View Controllers/Requests/FlagPostAlertViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class FlagPostAlertViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true)
     }
-    */
-
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
 }
