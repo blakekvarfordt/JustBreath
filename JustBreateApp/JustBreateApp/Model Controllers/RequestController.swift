62f3a64c4b5338da25c246524bdfcc7874e8634d
Cameron Stuart <apps@cameronstuart.com>
2019-10-09 11:07:38 -0600

    Adds cloudKit functionality for requests.

--- JustBreateApp/JustBreateApp/Model Controllers/RequestController.swift
+++ JustBreateApp/JustBreateApp/Model Controllers/RequestController.swift
@@ -13,7 +13,7 @@ class RequestController {
     
     static let shared = RequestController()
     
-    var requests = [Request]()
+    var requests: [Request] = []
     
     let publicDataBase = CKContainer.default().publicCloudDatabase
     
@@ -40,8 +40,7 @@ class RequestController {
     }
     
     func fetchRequests(completion: @escaping (Bool) -> Void) {
-        guard let currentUserRespondedTo = UserController.shared.currentUser?.respondedTo else { completion(false); return }
-        let predicate = NSPredicate(format: "\(RequestConstants.userReferenceKey) != %@", currentUserRespondedTo)
+        let predicate = NSPredicate(value: true)
         let query = CKQuery(recordType: RequestConstants.recordTypeKey, predicate: predicate)
         publicDataBase.perform(query, inZoneWith: nil) { (records, error) in
             
@@ -54,7 +53,16 @@ class RequestController {
             guard let records = records else { completion(false); return }
             let requests = records.compactMap({Request(ckRecord: $0)})
             
-            self.requests = requests
+            var filteredRequests: [Request] = []
+            
+            for request in requests {
+                guard let user = UserController.shared.currentUser else { completion(false); return }
+                if !user.respondedTo.contains(request.recordID.recordName) {
+                    filteredRequests.append(request)
+                }
+            }
+            
+            self.requests = filteredRequests
             completion(true)
         }
     }
